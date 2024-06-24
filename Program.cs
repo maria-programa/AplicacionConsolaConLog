ff47ab472ddbbed0294df7660da026845d7ccc28
maria-programa <mlozanoovejero314@gmail.com>
2024-06-24 14:19:46 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+using System;
+
+namespace $safeprojectname$
+{
+    class Program
+    {
+        static ILogger<Program> logger;
+
+        static void Main(string[] args)
+        {
+            //https://andrewlock.net/using-dependency-injection-in-a-net-core-console-application/
+            //setup DI
+            var services = new ServiceCollection();
+
+            //add logging
+            services.AddLogging();
+
+            //build the provider;
+            var serviceProvider = services.BuildServiceProvider();
+
+            //configure console logging
+            serviceProvider
+                .GetService<ILoggerFactory>()
+                .AddConsole(LogLevel.Trace, true);
+
+            //configure nlog
+            var loggerFactory = serviceProvider.GetService<ILoggerFactory>();
+            loggerFactory.AddNLog();
+            loggerFactory.ConfigureNLog("nlog.config");
+
+            //create a logger
+            logger = serviceProvider.GetService<ILoggerFactory>()
+                .CreateLogger<Program>();
+
+            logger.LogInformation("Hello World!");
+        }
+    }
+}
